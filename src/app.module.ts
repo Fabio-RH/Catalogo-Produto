2407998a2677c146965d07e831ea6e8a63572989
Fabio-RH <binhorh@hotmail.com>
2026-04-29 21:41:25 -0300

    Montando o esqueleto e organizando as pastas

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoriaModule } from './categoria/categoria.module';
+import { EstoqueModule } from './estoque/estoque.module';
+import { ImagemModule } from './imagem/imagem.module';
+import { ProdutoModule } from './produto/produto.module';
+
+@Module({
+  imports: [CategoriaModule, EstoqueModule, ImagemModule, ProdutoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
